cfaac2eefae2cb19025858ccc4fdd7f8cf2fac6e
black <black@example.com>
2026-07-10 13:17:30 +0500

    Call greeting functions

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 def greet_white():
     print("hello white")
 
+
 def greet_black():
     print("hello black")
 
+
 def main():
     print("hello everyone")
+    greet_white()
+    greet_black()
 
 
 if __name__ == "__main__":
